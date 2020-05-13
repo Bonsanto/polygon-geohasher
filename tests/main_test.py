2cefa6e49ab3da565a2fe2e7c0081880cf0b056a
Francisco J. Piedrahíta-Vélez <francisco.piedrahita@rappi.com>
2020-05-13 06:31:49 -0500

    Formated python files with black

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,10 @@
 import unittest
 
-from polygon_geohasher.polygon_geohasher import polygon_to_geohashes, geohash_to_polygon, geohashes_to_polygon
+from polygon_geohasher.polygon_geohasher import (
+    polygon_to_geohashes,
+    geohash_to_polygon,
+    geohashes_to_polygon,
+)
 from shapely import geometry
 
 
@@ -15,17 +19,25 @@ class TestSimpleMethods(unittest.TestCase):
         self.assertTrue(test_polygon.intersection(polygon).covers(test_polygon))
 
     def test_triangle(self):
-        test_polygon = geometry.Polygon([(-99.1795917, 19.432134), (-99.1656847, 19.429034),
-                                         (-99.1776492, 19.414236), (-99.1795917, 19.432134)])
+        test_polygon = geometry.Polygon(
+            [
+                (-99.1795917, 19.432134),
+                (-99.1656847, 19.429034),
+                (-99.1776492, 19.414236),
+                (-99.1795917, 19.432134),
+            ]
+        )
 
         polygon = geohashes_to_polygon(polygon_to_geohashes(test_polygon, 7))
         self.assertTrue(polygon.area <= test_polygon.area)
-        self.assertTrue(all(polygon.covers(geometry.Point(c)) for c in polygon.boundary.coords))
+        self.assertTrue(
+            all(polygon.covers(geometry.Point(c)) for c in polygon.boundary.coords)
+        )
 
         polygon = geohashes_to_polygon(polygon_to_geohashes(test_polygon, 7, False))
 
         self.assertTrue(polygon.area >= test_polygon.area)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
